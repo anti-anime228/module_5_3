773d56f8bdfc3a6cc29559e944b0f527ffd22434
anti-anime228 <qwerty9228@bk.ru>
2024-07-11 12:54:48 +0300

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -23,10 +23,33 @@ class House:
 
     def __add__(self, value):
         if isinstance(value, int):
-            return f"Название: {self.name}, кол-во этажей: {self.number_of_floors + value}"
-        return False
+            self.number_of_floors += value
+            return self
 
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
 
+    def __radd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
 
 
 h1 = House('ЖК Эльбрус', 10)
@@ -35,9 +58,24 @@ h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
 
-print(h1 == h2) # __eq__
+print(h1 == h2)  # __eq__
 
-h1 = h1 + 10 # __add__
+h1 = h1 + 10  # __add__
 print(h1)
-print(h2)
 print(h1 == h2)
+
+h1 += 10  # __iadd__
+print(h1)
+
+h2 = 10 + h2  # __radd__
+print(h2)
+
+print(h1 > h2)  # __gt__
+
+print(h1 >= h2)  # __ge__
+
+print(h1 < h2)  # __lt__
+
+print(h1 <= h2)  # __le__
+
+print(h1 != h2)  # __ne__
